f1ad803926b4aafc248144745989ac5a2c7d7661
natchaneeya <natchaneeya.s@ku.th>
2017-12-13 16:02:22 +0700

    HR main page (not complete)

---
+++ src/main/java/HR/controllers/ApplicantInfoController.java
@@ -0,0 +1,37 @@
+package HR.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.layout.FlowPane;
+
+public class ApplicantInfoController {
+    private MainController mainCtrl;
+    private FlowPane mainPane;
+    @FXML
+    private Button moreInfoButton, uploadInfoButton, changeStatusButton;
+
+    public void initialize(){
+
+    }
+    public void moreInfo(){
+
+    }
+    public void uploadInfo(){
+
+    }
+    public void changeStatus(){
+
+    }
+    public FlowPane getMainPane() {
+        return mainPane;
+    }
+
+    public void setMainPane(FlowPane mainPane) {
+        this.mainPane = mainPane;
+    }
+
+    public void setMainCtrl(MainController mainCtrl) {
+        this.mainCtrl = mainCtrl;
+    }
+}
