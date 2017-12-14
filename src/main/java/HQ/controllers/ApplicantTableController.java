1562ddbabce7a43be10ad7e76f6af3bf96545734
Aminomous <yoyothunder@gmail.com>
2017-12-14 10:51:34 +0700

    Bind Applicant data to TableColumn

--- src/main/java/HQ/controllers/ApplicantTableController.java
+++ src/main/java/HQ/controllers/ApplicantTableController.java
@@ -3,7 +3,10 @@ package HQ.controllers;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.FlowPane;
+
+import javax.swing.text.TableView;
 
 public class ApplicantTableController {
     private MainController mainCtrl;
@@ -12,7 +15,11 @@ public class ApplicantTableController {
     private TableColumn idColumn, firstnameColumn, lastnameColumn, positionColoumn, statusColumn;
 
     public void initialize() {
-
+        idColumn.setCellValueFactory(new PropertyValueFactory<ApplicantData, String>("id"));
+        firstnameColumn.setCellValueFactory(new PropertyValueFactory<ApplicantData, String>("firstName"));
+        lastnameColumn.setCellValueFactory(new PropertyValueFactory<ApplicantData, String>("lastName"));
+        positionColoumn.setCellValueFactory(new PropertyValueFactory<ApplicantData, String>("position"));
+        statusColumn.setCellValueFactory(new PropertyValueFactory<ApplicantData, String>("status"));
 
     }
 
@@ -32,14 +39,14 @@ public class ApplicantTableController {
         private SimpleStringProperty id;
         private SimpleStringProperty firstName;
         private SimpleStringProperty lastName;
-        private SimpleStringProperty salary;
+        private SimpleStringProperty position;
         private SimpleStringProperty status;
 
-        ApplicantData(String id, String firstName, String lastName, double salary, String status) {
+        ApplicantData(String id, String firstName, String lastName, String position, String status) {
             this.id = new SimpleStringProperty(id);
             this.firstName = new SimpleStringProperty(firstName);
             this.lastName = new SimpleStringProperty(lastName);
-            this.salary = new SimpleStringProperty(salary + "");
+            this.position = new SimpleStringProperty(position + "");
             this.status = new SimpleStringProperty(status);
         }
 
@@ -67,14 +74,6 @@ public class ApplicantTableController {
             return lastName;
         }
 
-        public String getSalary() {
-            return salary.get();
-        }
-
-        public SimpleStringProperty salaryProperty() {
-            return salary;
-        }
-
         public String getStatus() {
             return status.get();
         }
@@ -85,6 +84,18 @@ public class ApplicantTableController {
 
         public void setStatus(String status) {
             this.status.set(status);
+        }
+
+        public String getPosition() {
+            return position.get();
+        }
+
+        public SimpleStringProperty positionProperty() {
+            return position;
+        }
+
+        public void setPosition(String position) {
+            this.position.set(position);
         }
     }
 }
