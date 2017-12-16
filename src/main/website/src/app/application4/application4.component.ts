b5aabfc8631003e119f8a118deab19eced0ee1fa
jsice <w14chin@gmail.com>
2017-12-16 11:45:03 +0700

    sad php

--- src/main/website/src/app/application4/application4.component.ts
+++ src/main/website/src/app/application4/application4.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Application } from 'app/models/application';
+import { ApplicationService } from 'app/application.service';
 
 @Component({
   selector: 'app-application4',
@@ -8,8 +10,13 @@ import { Component, OnInit } from '@angular/core';
 export class Application4Component implements OnInit {
 
   title = "ประวัติการศึกษา (Educational Background)"
-
-  constructor() { }
+  application: Application
+  
+    constructor(private applicationService:ApplicationService) { 
+      this.applicationService.getApplication().subscribe((application) => {
+        this.application = application
+      })
+    }
 
   ngOnInit() {
   }
