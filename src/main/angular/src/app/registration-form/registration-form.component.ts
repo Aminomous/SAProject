faa74c06116fa71ce880d09435c9b38b204113bb
Aminomous <yoyothunder@gmail.com>
2018-01-22 13:54:15 +0700

    regis page 1

--- src/main/angular/src/app/registration-form/registration-form.component.ts
+++ src/main/angular/src/app/registration-form/registration-form.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from '../user.service';
+import { PersonalInformation } from '../models/personalInformation';
+
 
 @Component({
   selector: 'app-registration-form',
@@ -6,10 +10,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registration-form.component.css']
 })
 export class RegistrationFormComponent implements OnInit {
-
-  constructor() { }
+  pi: PersonalInformation
+  page
+  constructor(private router:Router, private userService: UserService) {
+    this.pi = this.userService.personalInformation
+    
+   }
 
   ngOnInit() {
+    let urls = this.router.url.split("/")
+    
+    if (urls[urls.length-1]=="") {
+      this.page = 1
+    } else {
+      this.page = +urls[urls.length-1]
+    }
+  }
+
+  save() {
+    this.userService.updatePersonalInformation()
+    this.router.navigate(['/'])
+  }
+
+  next() {
+    this.page+=1
+    this.router.navigate(['main','registration',this.page])
+  }
+
+  previous() {
+    this.page-=1
+    this.router.navigate(['main','registration',this.page])
+  }
+
+  cancel() {
+    this.router.navigate([''])
   }
 
 }
