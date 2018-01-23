230e45f720c334c95ba1e85e8055307448f28994
Aminomous <yoyothunder@gmail.com>
2018-01-23 10:15:47 +0700

    push by ice

--- src/main/angular/src/app/information5/information5.component.ts
+++ src/main/angular/src/app/information5/information5.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { PersonalInformation } from 'app/models/personalInformation';
+import { LanguageAbility } from 'app/models/languageAbility';
+import { UserService } from 'app/user.service';
 
 @Component({
   selector: 'app-information5',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./information5.component.css']
 })
 export class Information5Component implements OnInit {
-
-  constructor() { }
-
+  
+  pi: PersonalInformation 
+  constructor(private userService: UserService) { 
+    this.pi = new PersonalInformation()
+    this.userService.getPersonalInformation().then((pi)=>{
+      this.pi = pi
+      console.log(pi)
+    })
+  }
+  
+  
+  
   ngOnInit() {
   }
-
+  printOut(){
+    console.log(this.pi.word)
+  }
 }
--- src/main/angular/src/app/models/referencePerson.ts
+++ src/main/angular/src/app/models/referencePerson.ts
@@ -2,5 +2,5 @@ export class ReferencePerson {
     name: string;
     relationship: string;
     address: string;
-    phoneNumber: string;
+    telephone: string;
 }
