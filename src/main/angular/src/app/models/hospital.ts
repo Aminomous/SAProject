5d35c53912371725ae8e57d068466d65bb4edc13
jsice <w14chin@gmail.com>
2018-01-24 03:02:25 +0700

    edit regis-form1

--- src/main/angular/src/app/models/hospital.ts
+++ src/main/angular/src/app/models/hospital.ts
@@ -1,3 +1,4 @@
 export class Hospital {
     hospitalName: string;
+    constructor(name) {this.hospitalName = name}
 }
--- src/main/angular/src/app/registration-form1/registration-form1.component.ts
+++ src/main/angular/src/app/registration-form1/registration-form1.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'app/user.service';
 import { Application } from 'app/models/application';
+import { Hospital } from 'app/models/hospital';
 
 @Component({
   selector: 'app-registration-form1',
@@ -11,11 +12,34 @@ export class RegistrationForm1Component implements OnInit {
 
 
   application: Application = new Application()
+  hospitals = [
+    {hospital: new Hospital("โรงพยาบาลพญาไท 1"), selected: false},
+    {hospital: new Hospital("โรงพยาบาลพญาไท 2"), selected: false},
+    {hospital: new Hospital("โรงพยาบาลพญาไท 3"), selected: false},
+    {hospital: new Hospital("โรงพยาบาลพญาไท นวมินทร์"), selected: false},
+    {hospital: new Hospital("โรงพยาบาลพญาไท ศรีราชา"), selected: false},
+    {hospital: new Hospital("โรงพยาบาลพญาไท พหลโยธิน"), selected: false},
+    {hospital: new Hospital("โรงพยาบาลพญาไท สมุทรปราการ"), selected: false},
+    {hospital: new Hospital("โรงพยาบาลเปาโล โชคชัย 4"), selected: false},
+    {hospital: new Hospital("โรงพยาบาลเปาโล รังสิต"), selected: false},
+    {hospital: new Hospital("โรงพยาบาลเปาโล เกษตร"), selected: false}
+  ]
+  
+
+
   constructor(private userService: UserService) { 
     this.userService.personalInformation.subscribe(()=>{
       this.userService.application.subscribe(app=>{
         this.application = app
-        
+        for (let hos of this.application.hospitals) {
+          let name = hos.hospitalName
+          for (let hos2 of this.hospitals) {
+            if (hos2.hospital.hospitalName == name) {
+              hos2.selected = true
+              break
+            }
+          }
+        }
       })
     })
   }
@@ -23,14 +47,20 @@ export class RegistrationForm1Component implements OnInit {
   ngOnInit() {
   }
 
-  selectHospital(hos){
-
-    
-    let i = this.application.hospitals.indexOf(hos)
-    if (i==-1) {
-      this.application.hospitals.push(hos)
+  selectHospital(hosNo){
+    console.log(this.application)
+    if (this.hospitals[hosNo].selected) {
+      this.application.hospitals.push(this.hospitals[hosNo].hospital)
     } else {
-      this.application.hospitals.splice(i,1)
+      let name = this.hospitals[hosNo].hospital.hospitalName
+      let index = 0
+      for (let hos of this.application.hospitals) {
+        if (hos.hospitalName == name) {
+          break
+        }
+        index += 1
+      }
+      this.application.hospitals.splice(index, 1)
     }
   }
 
